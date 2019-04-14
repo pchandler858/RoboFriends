8687e0d9275a86f4ef61aa8bfd6bef49d86c2419
Preston Chandler <pchandler858@gmail.com>
2019-04-14 16:50:45 -0700

    Updates

---
+++ precache-manifest.5962e0d3ed2d736069fc096a8bccdf27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "e46a4f23eb38955a84d9",
+    "url": "/RoboFriends/static/js/main.b68903b6.chunk.js"
+  },
+  {
+    "revision": "5144afca390c09b00719",
+    "url": "/RoboFriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "e46a4f23eb38955a84d9",
+    "url": "/RoboFriends/static/css/main.694d389d.chunk.css"
+  },
+  {
+    "revision": "5144afca390c09b00719",
+    "url": "/RoboFriends/static/css/2.00e7d522.chunk.css"
+  },
+  {
+    "revision": "7a2cc6a88a54838ef697cd6ef3950d2b",
+    "url": "/RoboFriends/index.html"
+  }
+];
